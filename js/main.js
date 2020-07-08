6065dab2926b76f8fbff6f244076c49a26e72561
rligerko <ligerkoroman@gmail.com>
2020-07-08 14:06:39 +0300

    add header, main-info

---
+++ js/main.js
@@ -0,0 +1,44 @@
+$(function(){
+    $('.main__info-button-3').on('click', function(){
+        $('.main__info-photos').toggleClass('show');
+        $('.slick-slider').toggleClass('show-slider');
+    });
+      
+    $('.main__info-button').on('click', function(){
+        if($(this).hasClass('btn-active')){
+            $(this).removeClass('btn-active');
+        }
+        else{
+            $('.main__info-button').removeClass('btn-active');
+            $(this).addClass('btn-active');
+        }
+    
+    });
+    $('.main__info-button-1, .main__info-button-2, .main__info-button-4').on('click', function(){
+        $('.main__info-photos').removeClass('show');
+    });
+
+    $('.main__info-photos').slick({
+        slidesToShow: 2.5,
+        slidesToSckroll: 1,
+        arrows: false,
+        infinite: false,
+
+    //    swipe: false,
+        // responsive:[
+        //     {
+        //         breakpoint: 1024,
+        //         settings: {
+
+        //         }
+        //     },
+        //     {
+        //         breakpoint: 640,
+        //         settings: {
+        //             arrows: false   
+
+        //         }
+        //     }
+        // ]
+    });
+});
